Evolution-X-Devices/device_samsung_exynos9820-common

exynos9820-common: Switch to dot product CPU variant
 * beyond1lte:/ $ cat /proc/cpuinfo | grep -m 1 Features
   Features: fp asimd evtstrm aes pmull sha1 sha2 crc32
   atomics fphp asimdhp cpuid asimdrdm dcpop asimddp

 * From this, it's evident that our CPU (Exynos 9820)
   supports all the features of ARMv8.2-DotProd [1].
   Moreover AOSP sets this arch variant in their generic
   cortex-a55 ART target as well [2]. This should unlock
   some optimizations in different code paths.

[1]: https://en.wikichip.org/wiki/arm/armv8#ARMv8_Extensions_and_Processor_Features
[2]: https://android.googlesource.com/device/generic/art/+/refs/heads/master/armv8_cortex_a55/BoardConfig.mk#23

Change-Id: I484bb2bcc181c62e29c04bc6e467e5edea8ac0d8

BoardConfigCommon.mk
@@ -23,7 +23,7 @@ include vendor/samsung/exynos9820-common/BoardConfigVendor.mk
 
 ## Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a75

LeDrew2017/android_device_samsung_universal2100-common

BoardConfigCommon.mk
@@ -32,7 +32,7 @@ TARGET_SOC := exynos2100
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a-dotprog
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76